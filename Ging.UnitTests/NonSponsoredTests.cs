ca98ffad6efb1be1866a36f44759bce0e2370b0b
Thomas Flynn <tflynn049@gmail.com>
2018-07-25 15:29:59 -0400

    Commit working Sponsored and NonSponsoredLinkTests

---
+++ Ging.UnitTests/NonSponsoredTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Ging.Biz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ging.UnitTests
+{
+    [TestClass]
+    public class NonSponsoredLinkTests
+    {
+        [TestMethod]
+        public void NonSponsoredLinkConstructorShouldPass()
+        {
+            //Arrange
+            var link = new NonSponsoredLink("foo.com", "something", DateTime.Today, LinkKind.Blog);
+
+            //Assert
+            Assert.AreEqual("foo.com", link.Url);
+            Assert.AreEqual("something", link.Description);
+            Assert.AreEqual(DateTime.Today, link.LastCrawl);
+            Assert.AreEqual(LinkKind.Blog, link.Kind);
+        }
+    }
+}
---
+++ Ging.UnitTests/SponsoredLinkTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Ging.Biz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ging.UnitTests
+{
+    [TestClass]
+    public class SponsoredLinkTests
+    {
+        [TestMethod]
+        public void SponsoredLinkConstructorShouldPass()
+        {
+            //Arrange
+            var link = new SponsoredLink("foo.com", "something", "some guy", 0.25M);
+
+            //Assert
+            Assert.AreEqual("foo.com", link.Url);
+            Assert.AreEqual("something", link.Description);
+            Assert.AreEqual("some guy", link.Sponsor);
+            Assert.AreEqual(0.25M, link.CostPerImpression);
+            Assert.AreEqual(0M, link.Balance);
+        }
+    }
+}
