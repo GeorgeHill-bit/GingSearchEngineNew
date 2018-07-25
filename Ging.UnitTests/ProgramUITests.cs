18ab8963e7aafbecd07f7150a7aa1a9f29ce2b6d
Thomas Flynn <tflynn049@gmail.com>
2018-07-25 14:32:10 -0400

    commit SearchEngineTests and ProgramUITests

---
+++ Ging.UnitTests/ProgramUITests.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using Ging.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ging.UnitTests
+{
+    [ExcludeFromCodeCoverage]
+    [TestClass]
+    public class ProgramUITest
+    {
+        [TestMethod]
+        public void ProgramUIAddSponsoredLinkWhenUserFollowsInstructionsShouldPass()
+        {
+            //Arrange
+            var mockConsole = new MockConsole(new string[] { "addlink", "y", "foo.com", "something", "some guy", "0.15" });
+            var programUI = new ProgramUI(mockConsole);
+
+            //Act
+            programUI.Run();
+
+            //Assert
+            var searchEngine = programUI.SearchEngine;
+            var outputText = mockConsole.Output;
+
+            StringAssert.Contains(outputText, "a sponsored link?");
+            StringAssert.Contains(outputText, "enter the link's url:");
+
+            Assert.AreEqual(
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the name of the sponsor: " +
+                "Please enter the cost per impression: $" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): ",
+                outputText);
+        }
+
+        [TestMethod]
+        public void ProgramUIAddNonSponsoredLinkWhenUserFollowsInstructionsShouldPass()
+        {
+            //Arrange
+            var mockConsole = new MockConsole(new string[] { "addlink", "n", "foo.com", "something", "06/18/2018", "1" });
+            var programUI = new ProgramUI(mockConsole);
+
+            //Act
+            programUI.Run();
+
+            //Assert
+            var searchEngine = programUI.SearchEngine;
+            var outputText = mockConsole.Output;
+            Assert.AreEqual(
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the last date the page was crawled (mm/dd/yyyy): " +
+                "1 = forum\n" + "2 = blog\n" + "3 = other\r\n" +
+                "Please select a site type: " +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): ",
+                outputText);
+        }
+
+        [TestMethod]
+        public void ProgramUIListLinksWhenUserHasEnteredLinks()
+        {
+            //Arrange
+            var mockConsole = new MockConsole(new string[]
+            {
+                "addlink", "y", "foo.com", "something", "some guy", "0.15", string.Empty,
+                "addlink", "n", "foo.com", "something", "06/18/2018", "1", string.Empty,
+                "listlinks"
+            });
+            var programUI = new ProgramUI(mockConsole);
+
+            //Act
+            programUI.Run();
+
+            //Assert
+            var searchEngine = programUI.SearchEngine;
+            var outputText = mockConsole.Output;
+            Assert.AreEqual(
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the name of the sponsor: " +
+                "Please enter the cost per impression: $" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the last date the page was crawled (mm/dd/yyyy): " +
+                "1 = forum\n" + "2 = blog\n" + "3 = other\r\n" +
+                "Please select a site type: " +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Type: Sponsored Link\n" +
+                "Url: foo.com\n" +
+                "Description: something\n" +
+                "Impressions: 0\n\r\n" +
+                "Type: Non-Sponsored Link\n" +
+                "Url: foo.com\n" +
+                "Description: something\n" +
+                "Impressions: 0\n\r\n" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): ",
+                outputText);
+        }
+
+        [TestMethod]
+        public void ProgramUISearchWhenUserHasAddedLinksShouldPass()
+        {
+            //Arrange
+            var mockConsole = new MockConsole(new string[]
+            {
+                "addlink", "y", "foo.com", "something", "some guy", "0.15", string.Empty,
+                "addlink", "n", "foo.com", "something", DateTime.Today.ToString("MM/dd/yyyy"), "1", string.Empty,
+                "search", "something"
+            });
+            var programUI = new ProgramUI(mockConsole);
+
+            //Act
+            programUI.Run();
+
+            //Assert
+            var searchEngine = programUI.SearchEngine;
+            var outputText = mockConsole.Output;
+
+            Assert.AreEqual(
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the name of the sponsor: " +
+                "Please enter the cost per impression: $" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the last date the page was crawled (mm/dd/yyyy): " +
+                "1 = forum\n" + "2 = blog\n" + "3 = other\r\n" +
+                "Please select a site type: " +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Please enter a search term: " +
+                "SPONSORED\n" + "foo.com\n\r\n" +
+                "foo.com\n" + $"Last Crawled: {DateTime.Today:MM/dd/yyyy}\n\r\n" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): ",
+                outputText);
+        }
+
+        [TestMethod]
+        public void ProgramUIFinancialsWhenUserHasAddedLinksShouldPass()
+        {
+            //Arrange
+            var mockConsole = new MockConsole(new string[]
+            {
+                "addlink", "y", "foo.com", "something", "some guy", "0.15", string.Empty,
+                "search", "something", string.Empty,
+                "financials"
+            });
+            var programUI = new ProgramUI(mockConsole);
+
+            //Act
+            programUI.Run();
+
+            //Assert
+            var searchEngine = programUI.SearchEngine;
+            var outputText = mockConsole.Output;
+            Assert.AreEqual(
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Is this a sponsored link? (y/n): " +
+                "Please enter the link's url: " +
+                "Please enter a description for the link: " +
+                "Please enter the name of the sponsor: " +
+                "Please enter the cost per impression: $" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Please enter a search term: " +
+                "SPONSORED\n" + "foo.com\n\r\n" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): " +
+                "Called Clear method." +
+                "Url: foo.com\n" + "Sponsor: some guy\n" + "Balance: 0.15\n\r\n" +
+                "PRESS ENTER TO RETURN TO MENU" +
+                "Called Clear method." +
+                "Command (addlink, listlinks, search, or financials): ",
+                outputText);
+        }
+    }
+}
---
+++ Ging.UnitTests/SearchEngineTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using Ging.Biz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ging.UnitTests
+{
+    [TestClass]
+    public class SearchEngineTests
+    {
+        [TestMethod]
+        public void GetAllLinksShouldPass()
+        {
+            //Arrange
+            var searchEngine = CreateEngine();
+
+            //Act
+            var results = searchEngine.GetAllLinks();
+
+            //Assert
+            Assert.AreEqual(
+                "Type: Sponsored Link\n" + "Url: fooshop.com\n" + "Description: some online store\n" + "Impressions: 0",
+                results[0]);
+            Assert.AreEqual(
+                "Type: Sponsored Link\n" + "Url: foostore.com\n" + "Description: probably losing to Amazon\n" + "Impressions: 0",
+                results[1]);
+            Assert.AreEqual(
+                "Type: Sponsored Link\n" + "Url: foolife.com\n" + "Description: definitely losing to Amazon\n" + "Impressions: 0",
+                results[2]);
+            Assert.AreEqual(
+                "Type: Non-Sponsored Link\n" + "Url: fooblog.com\n" + "Description: some blog\n" + "Impressions: 0",
+                results[3]);
+            Assert.AreEqual(
+                "Type: Non-Sponsored Link\n" + "Url: fooforum.com\n" + "Description: some forum\n" + "Impressions: 0",
+                results[4]);
+            Assert.AreEqual(
+                "Type: Non-Sponsored Link\n" + "Url: foobar.com\n" + "Description: something else\n" + "Impressions: 0",
+                results[5]);
+            Assert.AreEqual(6, results.Count);
+        }
+
+        [TestMethod]
+        public void SearchForSomeShouldPass()
+        {
+            //Arrange
+            var searchEngine = CreateEngine();
+
+            //Act
+            var results = searchEngine.Search("some");
+
+            //Assert
+            Assert.AreEqual(
+                "SPONSORED\n" + "fooshop.com",
+                results[0]);
+            Assert.AreEqual("fooblog.com\n" + $"Last Crawled: {DateTime.Today:MM/dd/yyyy}",
+                results[1]);
+            Assert.AreEqual("fooforum.com\n" + $"Last Crawled: {DateTime.Today:MM/dd/yyyy}",
+                results[2]);
+            Assert.AreEqual("foobar.com\n" + $"Last Crawled: {DateTime.Today:MM/dd/yyyy}",
+                results[3]);
+            Assert.AreEqual(4, results.Count);
+        }
+
+        [TestMethod]
+        public void LessThanFiveInFinancialsShouldPass()
+        {
+            //Arrange
+            var searchEngine = CreateEngine();
+
+            //Act
+            searchEngine.Search("amazon");
+            var results = searchEngine.GetFinancials();
+
+            //Assert
+            Assert.AreEqual(
+                "Url: foostore.com\n" + "Sponsor: some dude\n" + "Balance: 0.3",
+                results[0]);
+            Assert.AreEqual(
+                "Url: foolife.com\n" + "Sponsor: some hippie\n" + "Balance: 0.2",
+                results[1]);
+            Assert.AreEqual(2, results.Count);
+        }
+
+        [TestMethod]
+        public void MoreThanFiveFinancialsShouldPass()
+        {
+            //Arrange
+            var searchEngine = new SearchEngine();
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("fooshop.com", "something", "some guy", 0.25m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("foostore.com", "something", "some dude", 0.3m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("foolife.com", "something", "some hippie", 0.2m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("foobar.com", "something", "some bartender", 0.15m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("fooblog.com", "something", "some blogger", 0.25m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("fooforum.com", "something", "some forum admin", 0.4m));
+
+            //Act
+            searchEngine.Search("something");
+            var results = searchEngine.GetFinancials();
+
+            //Assert
+            Assert.AreEqual(5, results.Count);
+        }
+
+        [TestMethod]
+        public void NoResultsFinancialsShouldPass()
+        {
+            //Arrange
+            var searchEngine = CreateEngine();
+
+            //Act
+            var results = searchEngine.GetFinancials();
+
+            //Assert
+            Assert.AreEqual(0, results.Count);
+        }
+
+        private SearchEngine CreateEngine()
+        {
+            var searchEngine = new SearchEngine();
+
+            searchEngine.AddNonSponsoredLink(
+                new NonSponsoredLink("fooblog.com", "some blog", DateTime.Today, LinkKind.Blog));
+            searchEngine.AddNonSponsoredLink(
+                new NonSponsoredLink("fooforum.com", "some forum", DateTime.Today, LinkKind.Forum));
+            searchEngine.AddNonSponsoredLink(
+                new NonSponsoredLink("foobar.com", "something else", DateTime.Today, LinkKind.Forum));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("fooshop.com", "some online store", "some guy", 0.25m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("foostore.com", "probably losing to Amazon", "some dude", 0.3m));
+            searchEngine.AddSponsoredLink(
+                new SponsoredLink("foolife.com", "definitely losing to Amazon", "some hippie", 0.2m));
+
+            return searchEngine;
+        }
+    }
+}
+
